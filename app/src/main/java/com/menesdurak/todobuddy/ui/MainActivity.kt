13e1f0da023692b21fdac6923c72d683000ad53f
menesdurak <m.enes.durak@gmail.com>
2023-01-27 21:40:02 +0300

    Added view binding

--- app/src/main/java/com/menesdurak/todobuddy/ui/MainActivity.kt
+++ app/src/main/java/com/menesdurak/todobuddy/ui/MainActivity.kt
@@ -2,11 +2,15 @@ package com.menesdurak.todobuddy.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.menesdurak.todobuddy.R
+import com.menesdurak.todobuddy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
