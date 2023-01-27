237c56604352040ac6f4f967006c270b7ef0b817
menesdurak <m.enes.durak@gmail.com>
2023-01-27 21:56:28 +0300

    Created SignUpFragment.kt

---
+++ app/src/main/java/com/menesdurak/todobuddy/ui/SignUpFragment.kt
@@ -0,0 +1,31 @@
+package com.menesdurak.todobuddy.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.menesdurak.todobuddy.R
+import com.menesdurak.todobuddy.databinding.FragmentHomeBinding
+import com.menesdurak.todobuddy.databinding.FragmentSignUpBinding
+
+class SignUpFragment : Fragment() {
+
+    private var _binding: FragmentSignUpBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
