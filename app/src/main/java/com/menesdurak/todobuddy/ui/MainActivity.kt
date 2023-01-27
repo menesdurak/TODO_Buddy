e76d755de630908e7f918283780c87dc54f1c1fd
menesdurak <m.enes.durak@gmail.com>
2023-01-27 20:24:02 +0300

    First commit

---
+++ app/src/main/java/com/menesdurak/todobuddy/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.menesdurak.todobuddy.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.menesdurak.todobuddy.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
