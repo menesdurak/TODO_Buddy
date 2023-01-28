e260a1957819864db2b7115a8bd2eddf608d6e58
menesdurak <m.enes.durak@gmail.com>
2023-01-28 10:27:51 +0300

    Added sign up navigate

--- app/src/main/java/com/menesdurak/todobuddy/ui/LoginFragment.kt
+++ app/src/main/java/com/menesdurak/todobuddy/ui/LoginFragment.kt
@@ -5,11 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.menesdurak.todobuddy.R
 import com.menesdurak.todobuddy.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            Toast.makeText(requireContext(), "Already signed.", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +42,14 @@ class LoginFragment : Fragment() {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvSignUp.setOnClickListener {
+            findNavController().navigate(R.id.signUpFragment)
+        }
     }
 
     override fun onDestroyView() {
