6dfae176413a3f8b99f88ccc79155cd619a0bc97
menesdurak <m.enes.durak@gmail.com>
2023-01-28 10:27:28 +0300

    Added sign up codes

--- app/src/main/java/com/menesdurak/todobuddy/ui/SignUpFragment.kt
+++ app/src/main/java/com/menesdurak/todobuddy/ui/SignUpFragment.kt
@@ -1,10 +1,16 @@
 package com.menesdurak.todobuddy.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.menesdurak.todobuddy.R
 import com.menesdurak.todobuddy.databinding.FragmentHomeBinding
 import com.menesdurak.todobuddy.databinding.FragmentSignUpBinding
@@ -14,6 +20,17 @@ class SignUpFragment : Fragment() {
     private var _binding: FragmentSignUpBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var auth: FirebaseAuth
+    private lateinit var email: String
+    private lateinit var password: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,6 +39,31 @@ class SignUpFragment : Fragment() {
         _binding = FragmentSignUpBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnSignUp.setOnClickListener {
+
+            email = binding.etMail.text.toString()
+            password = binding.etPassword.text.toString()
+
+            if( email.isNotEmpty() && password.isNotEmpty()) {
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                    if(task.isSuccessful) {
+                        Log.d("signUpSuccess", "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        findNavController().navigate(R.id.loginFragment)
+                    } else {
+                        Log.w("signUpFail", "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(requireContext(), "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+
     }
 
     override fun onDestroyView() {
