4ec16ad0769669f5e8b808779f8b5f302c3a54e7
menesdurak <m.enes.durak@gmail.com>
2023-02-01 15:44:30 +0300

    Edited write and update functions

--- app/src/main/java/com/menesdurak/todobuddy/ui/HomeFragment.kt
+++ app/src/main/java/com/menesdurak/todobuddy/ui/HomeFragment.kt
@@ -33,47 +33,59 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+//-------------------------------------------------------------------------------
+        //CREATE DATABASE AND WRITE
         val database = Firebase.database
-//        val myRef = database.getReference("message")
         val notesRef = database.getReference("groups")
+        val notesRef2 = database.reference.child("path").child("111").child("222")
         val note1 = Note("123")
         val note2 = Note("abc", true)
         val note3 = Note("1a2b")
         val notesList = arrayListOf(note1, note2)
         val notesList2 = arrayListOf(note1, note2, note3)
+        val newGroupRef = notesRef.push()
+        val key = newGroupRef.key
+//        Log.e("1234", key!!)
+        val userList = arrayListOf("12abc", "ab123")
+        val newGroup = Group("Market", notesList, userList)
+        newGroupRef.setValue(newGroup)
 
-        //Creating new reference for note
-        val newRef = notesRef.push()
-        //Getting unique key of new reference
-        val key = newRef.key
-        Log.e("1234", key!!)
-        //Creating a new Group class
-        val group = Group(notesList2, key)
-        //Pushing new Group
+//        //Creating new reference for note
+//        val newRef = notesRef.push()
+//        //Getting unique key of new reference
+//        val key = newRef.key
+//        Log.e("1234", key!!)
+//        //Creating a new Group class
+//        val group = Group(notesList2, key)
+//        //Pushing new Group
 //        newRef.setValue(group)
 
-        val updateNote = HashMap<String, Any>()
-        updateNote["notes"] = Group(notesList)
-        notesRef.child("-NN6a8PtQ-L-rq1P5HJJ").updateChildren(updateNote)
+//------------------------------------------------------------------------------------
+        //UPDATE
+        val newNote = Note("1aaaaa")
+        val newList = notesList.add(newNote)
+        val updateUsers = hashMapOf<String, Any>(
+            "userIds" to "abc123a"
+        )
+        userList.add("3456")
+//        notesRef.child("-NNBp0ELFnsHt4fvLFWZ").updateChildren(updateUsers)
+//        notesRef.child("-NNBshFWr-YjutIivAzn").child("userIds").setValue(userList)
+
 //-------------------------------------------------------------------------------------
-//        val userId = "1234"
-//        val listId = "1212"
-//        val userId2 = "12345"
-//        val listId2 = "12121"
-
-//        myRef.root.child(userId).child(listId).setValue("Test 1111")
-
-//        myRef.setValue("Hello, World!")
-
+        //READ
 //        notesRef.addValueEventListener(object : ValueEventListener {
 //            override fun onDataChange(snapshot: DataSnapshot) {
 //                for(i in snapshot.children) {
 //                    val group = i.getValue(Group::class.java)
 //                    if (group != null) {
 //                        Log.e("******","******")
+//                        Log.e("Key", "${i.key}")
 //                        for (j in group.notes!!){
 //                            Log.e("Note", j.note!!)
 //                            Log.e("isDrawn", "${j.drawn}")
+//                        }
+//                        for(j in group.userIds!!) {
+//                            Log.e("UserID", j)
 //                        }
 //                    }
 //                }
