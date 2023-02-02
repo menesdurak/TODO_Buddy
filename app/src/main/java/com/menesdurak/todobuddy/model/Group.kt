0be86fa0934d52226d2e52c068f480858f3c2893
menesdurak <m.enes.durak@gmail.com>
2023-02-02 19:22:43 +0300

    Added key to model

--- app/src/main/java/com/menesdurak/todobuddy/model/Group.kt
+++ app/src/main/java/com/menesdurak/todobuddy/model/Group.kt
@@ -1,7 +1,11 @@
 package com.menesdurak.todobuddy.model
 
 import com.google.firebase.database.IgnoreExtraProperties
+
 @IgnoreExtraProperties
-data class Group(val title:String? = null,
-                 val notes: ArrayList<Note>? = null,
-                 val userIds: ArrayList<String>? = null)
+data class Group(
+    val title: String? = null,
+    val notes: ArrayList<Note>? = null,
+    val userIds: ArrayList<String>? = null,
+    val key: String? = null
+)
