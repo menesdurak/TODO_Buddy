dec2b3b1baa5e0b00ee72006a05353f6c77f886d
menesdurak <m.enes.durak@gmail.com>
2023-02-03 13:23:45 +0300

    Created Add Note Fragment

---
+++ app/src/main/java/com/menesdurak/todobuddy/ui/note/AddNoteFragment.kt
@@ -0,0 +1,23 @@
+package com.menesdurak.todobuddy.ui.note
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.menesdurak.todobuddy.R
+import com.menesdurak.todobuddy.databinding.FragmentNoteBinding
+
+class AddNoteFragment : Fragment() {
+
+    private var _binding: FragmentNoteBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_note, container, false)
+    }
+}
