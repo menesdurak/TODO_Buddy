0e5104a05b17f6c184f72a83aa931138f9f40c36
menesdurak <m.enes.durak@gmail.com>
2023-02-03 11:34:07 +0300

    Added navigation to Note Fragment

--- app/src/main/java/com/menesdurak/todobuddy/ui/home/HomeFragment.kt
+++ app/src/main/java/com/menesdurak/todobuddy/ui/home/HomeFragment.kt
@@ -1,19 +1,15 @@
 package com.menesdurak.todobuddy.ui.home
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.menesdurak.todobuddy.databinding.FragmentHomeBinding
@@ -135,7 +131,8 @@ class HomeFragment : Fragment() {
             binding.recyclerView.adapter = homeAdapter
             homeAdapter.setOnItemClickListener(object : HomeAdapter.HomeListClickListener {
                 override fun onItemClicked(position: Int) {
-                    println(keyList[position])
+                    val action = HomeFragmentDirections.actionHomeFragmentToNoteFragment(keyList[position])
+                    findNavController().navigate(action)
                 }
             })
         }
