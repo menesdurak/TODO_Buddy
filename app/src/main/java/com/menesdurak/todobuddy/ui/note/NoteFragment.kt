103a162f8083a14ea3117fd25b0322d3873ce290
menesdurak <m.enes.durak@gmail.com>
2023-02-03 11:32:04 +0300

    Added dummy data to Note Fragment

--- app/src/main/java/com/menesdurak/todobuddy/ui/note/NoteFragment.kt
+++ app/src/main/java/com/menesdurak/todobuddy/ui/note/NoteFragment.kt
@@ -5,21 +5,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.menesdurak.todobuddy.R
 import com.menesdurak.todobuddy.databinding.FragmentLoginBinding
+import com.menesdurak.todobuddy.databinding.FragmentNoteBinding
 
 class NoteFragment : Fragment() {
 
-    private var _binding: FragmentLoginBinding? = null
+    private var _binding: FragmentNoteBinding? = null
     private val binding get() = _binding!!
+    private var groupKey: String = "0"
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        _binding = FragmentNoteBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        val args: NoteFragmentArgs by navArgs()
+        groupKey = args.groupKey
+
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvKey.text = groupKey
     }
 }
