dbf7ab1b6f251b2a106d7ed9f3224e7733aeb6bd
menesdurak <m.enes.durak@gmail.com>
2023-02-04 08:48:11 +0300

    Added test codes

--- app/src/main/java/com/menesdurak/todobuddy/ui/note/AddNoteFragment.kt
+++ app/src/main/java/com/menesdurak/todobuddy/ui/note/AddNoteFragment.kt
@@ -5,19 +5,69 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.menesdurak.todobuddy.R
+import com.menesdurak.todobuddy.databinding.FragmentAddNoteBinding
+import com.menesdurak.todobuddy.databinding.FragmentHomeBinding
 import com.menesdurak.todobuddy.databinding.FragmentNoteBinding
+import com.menesdurak.todobuddy.model.Group
+import com.menesdurak.todobuddy.model.Note
 
 class AddNoteFragment : Fragment() {
 
-    private var _binding: FragmentNoteBinding? = null
+    private var _binding: FragmentAddNoteBinding? = null
     private val binding get() = _binding!!
 
+    private var groupKey: String = "0"
+    private lateinit var database: FirebaseDatabase
+    private lateinit var noteRef: DatabaseReference
+    private lateinit var notesList: ArrayList<String>
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_note, container, false)
+        _binding = FragmentAddNoteBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        groupKey = arguments?.getString("key").toString()
+
+        //CREATE DATABASE AND WRITE
+        database = Firebase.database
+        noteRef = database.getReference("groups").child(groupKey).child("notes")
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val args: AddNoteFragmentArgs by navArgs()
+        val key = args.key
+
+        binding.btnAdd.setOnClickListener {
+            val newNoteNote = binding.etNote.text.toString()
+            val newNote = Note(newNoteNote)
+            if (newNoteNote.isNotEmpty()) {
+                notesList = arrayListOf()
+
+                noteRef.get().addOnSuccessListener {
+                    for (i in it.children) {
+                        val note = i.getValue(Note::class.java)
+                        println(note!!.note)
+                    }
+                }
+                val push = noteRef.push()
+                push.setValue(newNote)
+//                println(notesList)
+//                val action = AddNoteFragmentDirections.actionAddNoteFragmentToNoteFragment(key)
+//                findNavController().navigate(action)
+            }
+        }
     }
 }
