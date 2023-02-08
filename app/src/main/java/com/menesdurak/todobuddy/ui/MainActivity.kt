4f25461071ec9ea3e864e3bb4d4a4d41204879f6
menesdurak <m.enes.durak@gmail.com>
2023-02-08 18:51:43 +0300

    Added options menu to main activity

--- app/src/main/java/com/menesdurak/todobuddy/ui/MainActivity.kt
+++ app/src/main/java/com/menesdurak/todobuddy/ui/MainActivity.kt
@@ -2,17 +2,47 @@ package com.menesdurak.todobuddy.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.app
 import com.menesdurak.todobuddy.R
 import com.menesdurak.todobuddy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var navHostFragment : NavHostFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        auth = Firebase.auth
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_options, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.sign_out -> {
+                auth.signOut()
+                navHostFragment.navController.navigate(R.id.loginFragment)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
