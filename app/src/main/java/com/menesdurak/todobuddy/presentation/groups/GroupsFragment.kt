ac7fda66279d1e61fb6918817148875dce45135d
menesdurak <m.enes.durak@gmail.com>
2023-07-06 08:46:26 +0300

    Created Groups Fragment

---
+++ app/src/main/java/com/menesdurak/todobuddy/presentation/groups/GroupsFragment.kt
@@ -0,0 +1,45 @@
+package com.menesdurak.todobuddy.presentation.groups
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.NavArgs
+import androidx.navigation.fragment.navArgs
+import com.menesdurak.todobuddy.R
+import com.menesdurak.todobuddy.databinding.FragmentGroupsBinding
+
+class GroupsFragment : Fragment() {
+    private var _binding: FragmentGroupsBinding? = null
+    private val binding get() = _binding!!
+
+    private var email = ""
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentGroupsBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        //Receiving arguments
+        val args: GroupsFragmentArgs by navArgs()
+        email = args.email
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Toast.makeText(requireContext(), email, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
